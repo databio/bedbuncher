0e56d8f9d83008eae1c7abf5a2e60d6cf10273a8
Jose E. Verdezoto Mosquera <josev4@uw.edu>
2020-01-22 16:42:30 -0500

    fix odd spacing issues

--- bedbuncher_ESlowclient.py
+++ bedbuncher_ESlowclient.py
@@ -16,7 +16,6 @@ import tarfile
 parser = ArgumentParser(
     description="A pipeline to produce sets of bed files (bedsets) from bedbase")
 
-
 parser.add_argument("-i", "--index", help="name of ES database index where files are stored", type=str)
 parser.add_argument("-q", "--query", help="what variable to perform to search in", type=str)
 parser.add_argument("-d", "--dbhost", help="this should be the database host address we need to connect to", default="localhost" )
@@ -33,7 +32,7 @@ out_parent = args.output_parent
 
 
 def main():
-	pm = pypiper.PipelineManager(name="bedbuncher-pipeline", outfolder=out_parent, args=args)
+    pm = pypiper.PipelineManager(name="bedbuncher-pipeline", outfolder=out_parent, args=args)
 
 # ESTABLISH CONTACT WITH ELASTIC SEARCH AND PROVIDE THE FILTER CRITERIA TO GET THE SET OF BED FILES
 # bed files were already inserted into the database using es.index
@@ -46,25 +45,21 @@ def main():
 			print("Connected to elasticsearch cluster", es.info())
 			search_result = es.search(index=args.index, body=args.query)
 			if not search_result['timed_out']: 
-				print("the query {} returned {} hits".format(args.query, search_result['hits']['total']['value']))
-				
+				print("the query {} returned {} hits".format(args.query, search_result['hits']['total']['value']))				
 				# need to iterate through the returned dictionary to find files paths
 				search_hits = search_result['hits']['hits']
-
 				# Alternative to tar using the CML: TAR files using the tarfile module
-	            tar_archive = tarfile.open(args.bedset_name, mode=w:gz)
-	            for files in search_hits:
-	            	# need to get access to bed json file to get the paths ['_source']
-	            	bed_source = files['_source'].get("sample_name") # this should point me to the raw bed file
+	            		tar_archive = tarfile.open(args.bedset_name, mode=w:gz)
+	            		for files in search_hits:
+	            			# need to get access to bed json file to get the paths ['_source']
+	            			bed_source = files['_source'].get("sample_name") # this should point me to the raw bed file
 					tar_archive.add(bed_source)
-				tar_archive.close()
+					tar_archive.close()
 			else:
 				print("The provided query doesn't match the database record")
-
 	except Exception as ex:
 		print("error while setting connection", ex) 
 
-	
 	# Create df with bedfiles metadata: gc_content, num_regions, mean_abs_tss_dist, genomic_partitions
 	bedstats_df = pd.DataFrame(columns=['BEDfile_id', 'GC_Content', 'Regions_number', 'Distance_from_feature', 
 						'Exon_frequency', 'Exon_percentage', 
@@ -80,19 +75,16 @@ def main():
 	# iterate through the ['hits']['hits']['_source'] attribute of the bedset
 	for bed_file in search_hits:
 		source = bed_file['_source']
-		
 		# get GenomicDIstributions data for each bed file as described in JSON file
 		file_id = source.get("id")
 		gc_content = source.get("gc_content")
 		regions_number = source.get("num_regions")
-		feat_distance = source.get("mean_abs_tss_dist")
-		
+		feat_distance = source.get("mean_abs_tss_dist")		
 		bedstats_df = bedstats_df.append({'BEDfile_id': make_float(file_id), 
 						'GC_Content': make_float(gc_content), 
 						'Regions_number': make_float(regions_number),
 						'Distance_from_feature':make_float(feat_distance)},
 										ignore_index=True)
-		
 		# iterate through the genomic partitions list to get feaures like exon and intron with stats (each list has several dictionaries)
 		genomic_partitions = source.get("genomic_partitions")
 		for item in genomic_partitions:
@@ -100,7 +92,7 @@ def main():
 			partition_frequency = item.get("Freq")
 			partition_percent = item.get("Perc")
 			if partition_id == 'exon':
-				bedstats_df = bedstats_df.append({'Exon_frequency': partition_frequency, 
+			    bedstats_df = bedstats_df.append({'Exon_frequency': partition_frequency, 
 								'Exon_percentage': partition_percent}, ignore_index=True)		
 			elif partition_id == 'intergenic': 
 				bedstats_df = bedstats_df.append({'Intergenic_frequency': partition_frequency, 
