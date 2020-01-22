3cb3b30387c867ac6f7f71d93e7840e6fc691187
joseverdezoto <j.esiempreliga@hotmail.com>
2020-01-22 16:21:25 -0500

    create dataframe with ES response object metadata

--- bedbuncher_ESlowclient.py
+++ bedbuncher_ESlowclient.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
 import pypiper, os, sys
+import pandas as pd
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 from elasticsearch_dsl import MultiSearch, Search
-import json, gzip, shutil, yaml
 import tarfile
 
 # steps needed to create the pipeline
@@ -24,7 +24,7 @@ parser.add_argument("-p", "--port", help="port number to set connection to elast
 
 # add pypiper args to make pipeline looper compatible
 parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "looper"],
-                                            required=["--field", "--value"])
+                                            required=["--index", "--query"])
 args = parser.parse_args()
 
 # SET OUTPUT FOLDER
@@ -36,9 +36,8 @@ def main():
 	pm = pypiper.PipelineManager(name="bedbuncher-pipeline", outfolder=out_parent, args=args)
 
 # ESTABLISH CONTACT WITH ELASTIC SEARCH AND PROVIDE THE FILTER CRITERIA TO GET THE SET OF BED FILES
-#  In Elasticsearch you index, search,sort and filter documents.
 # bed files were already inserted into the database using es.index
-# Perform the search/retrieval using the low-level python elasticsearch_dsl
+# Perform the search/retrieval using the low-level elasticsearch python client
 
 # Open connection to the elastic cluster;
 	try:
@@ -46,7 +45,7 @@ def main():
 		if es is not None:
 			print("Connected to elasticsearch cluster", es.info())
 			search_result = es.search(index=args.index, body=args.query)
-			if search_result.success() == True: 
+			if not search_result['timed_out']: 
 				print("the query {} returned {} hits".format(args.query, search_result['hits']['total']['value']))
 				
 				# need to iterate through the returned dictionary to find files paths
@@ -55,7 +54,8 @@ def main():
 				# Alternative to tar using the CML: TAR files using the tarfile module
 	            tar_archive = tarfile.open(args.bedset_name, mode=w:gz)
 	            for files in search_hits:
-	            	bed_source = files['_source'].get("sample_name")
+	            	# need to get access to bed json file to get the paths ['_source']
+	            	bed_source = files['_source'].get("sample_name") # this should point me to the raw bed file
 					tar_archive.add(bed_source)
 				tar_archive.close()
 			else:
@@ -64,4 +64,61 @@ def main():
 	except Exception as ex:
 		print("error while setting connection", ex) 
 
-	# Need to create matrix with bedfiles metadata
+	
+	# Create df with bedfiles metadata: gc_content, num_regions, mean_abs_tss_dist, genomic_partitions
+	bedstats_df = pd.DataFrame(columns=['BEDfile_id', 'GC_Content', 'Regions_number', 'Distance_from_feature', 
+										'Exon_frequency', 'Exon_percentage', 
+										'Intergenic_frequency', 'Intergenic_percentage',
+										'Intron_frequency', 'Intron_percentage',
+										'PromoterCore_frequency', 'PromoterCore_percentage'
+										'PromoterProx_frequency', ' PromoterProx_percentage'])
+	
+	# transform individual stats from dictionary into floats to perform calculations
+	def make_float(es_element):
+		float(es_element[0])
+
+	# iterate through the ['hits']['hits']['_source'] attribute of the bedset
+	for bed_file in search_hits:
+		source = bed_file['_source']
+		
+		# get GenomicDIstributions data for each bed file as described in JSON file
+		file_id = source.get("id")
+		gc_content = source.get("gc_content")
+		regions_number = source.get("num_regions")
+		feat_distance = source.get("mean_abs_tss_dist")
+		
+		bedstats_df = bedstats_df.append({'BEDfile_id': make_float(file_id), 
+										'GC_Content': make_float(gc_content), 
+										'Regions_number': make_float(regions_number),
+										'Distance_from_feature':make_float(feat_distance)},
+										ignore_index=True)
+		
+		# iterate through the genomic partitions list to get feaures like exon and intron with stats (each list has several dictionaries)
+		genomic_partitions = source.get("genomic_partitions")
+		for item in genomic_partitions:
+			partition_id = item.get("partition")
+			partition_frequency = item.get("Freq")
+			partition_percent = item.get("Perc")
+			if partition_id == 'exon':
+				bedstats_df = bedstats_df.append({'Exon_frequency': partition_frequency, 
+												'Exon_percentage': partition_percent}, ignore_index=True)		
+			elif partition_id == 'intergenic': 
+				bedstats_df = bedstats_df.append({'Intergenic_frequency': partition_frequency, 
+												'Intergenic_percentage': partition_percent}, ignore_index=True)				
+			elif partition_id == 'intron':
+				bedstats_df = bedstats_df.append({'Intron_frequency': partition_frequency, 
+												'Intron_percentage': partition_percent}, ignore_index=True)
+			elif partition_id == 'promoterCore':
+				bedstats_df = bedstats_df.append({'PromoterCore_frequency': partition_frequency, 
+												'PromoterCore_percentage': partition_percent}, ignore_index=True)
+			else:
+				partition_id == 'promoterProx':
+				bedstats_df = bedstats_df.append({'PromoterCore_frequency': partition_frequency, 
+												'PromoterCore_percentage': partition_percent}, ignore_index=True)
+
+	# Calculate average values for gc_content, num_regions and mean_abs_tss_dist 
+	avg_gc_content = bedstats_df["GC_Content"].mean()
+	avg_num_regions = bedstats_df["Regions_number"].mean()
+	avg_distance = bedstats_df["Distance_from_feature"].mean()
+		
+
